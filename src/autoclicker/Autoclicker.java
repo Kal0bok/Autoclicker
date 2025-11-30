ac30347a6b4c1dae911af985987301c7c8c2c5e1
Bogdan <bogdan1ptlvt@gmail.com>
2025-12-01 00:27:36 +0200

    Project base structure and Main class

--- src/autoclicker/Autoclicker.java
+++ src/autoclicker/Autoclicker.java
@@ -1,7 +1,18 @@
 package autoclicker;
 
-public class Autoclicker {
-	
-	//start
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
+public class Autoclicker {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getLookAndFeel());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        SwingUtilities.invokeLater(() -> {
+            new AutoClickerFrame().setVisible(true);
+        });
+    }
 }
